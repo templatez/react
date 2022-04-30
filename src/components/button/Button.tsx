e63d0976183027a691db09b87a53708462b8204c
tipup-manager <tipup-manager@gmail.com>
2022-04-30 21:51:02 +0900

    add to scss, stories

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-
-
+import './Button.scss';
 export interface ButtonProps {
   label: string;
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return (<div className="test">
+  <button>{props.label}</button>;
+  </div>)
 };
 
 export default Button;
